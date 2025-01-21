acdc078275a0c21bd9fc56de05c0eded6e176cda
ehthiede-student <eht45+student@cornell.edu>
2025-01-20 22:49:47 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,6 @@
 def hello_world():
     return "Hello World!"
 
-def add_two_numbers(a: float, b: float) -> float:
-    """
-    Evaluates the sum of two numbers and returns the result.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of a and b.
-
-    Example:
-        add_two_numbers(1.0, 2.0) -> 3.0
-
-    """
+def add_two_numbers(a, b):
     return a + b
 
