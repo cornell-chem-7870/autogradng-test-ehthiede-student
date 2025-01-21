54cce8cc43465dc95457a9cfa63b30218be82f82
ehthiede-student <eht45+student@cornell.edu>
2025-01-20 22:42:04 -0500

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -2,7 +2,7 @@ import hello;
 
 
 def test_add_two_floats(self):
-    assert hw_1.add_two_numbers(1.0, 2.4) == 3.4
-    assert hw_1.add_two_numbers(0.0, 0.0) == 0.0
-    assert hw_1.add_two_numbers(-1.0, -1.2) == -2.2
-    assert hw_1.add_two_numbers(1.0, -1.0) == 0.0
+    assert hello.add_two_numbers(1.0, 2.4) == 3.4
+    assert hello.add_two_numbers(0.0, 0.0) == 0.0
+    assert hello.add_two_numbers(-1.0, -1.2) == -2.2
+    assert hello.add_two_numbers(1.0, -1.0) == 0.0
