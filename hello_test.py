7a2d42e6994ce99a51c94f2b0a0f741ea820f92d
ehthiede-student <eht45+student@cornell.edu>
2025-01-20 22:39:28 -0500

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -1,4 +1,8 @@
-import hello;
+import hello
 
-def test_hello():
-    assert hello.hello_world() == "Hello World!"
+
+def test_add_two_floats(self):
+    assert hw_1.add_two_numbers(1.0, 2.4) == 3.4
+    assert hw_1.add_two_numbers(0.0, 0.0) == 0.0
+    assert hw_1.add_two_numbers(-1.0, -1.2) == -2.2
+    assert hw_1.add_two_numbers(1.0, -1.0) == 0.0
